e19108794c5b7789fccf5b9ab6b621a8aa3c11a9
Petre Vane <petre.vane@gmail.com>
2020-12-03 17:35:13 +0100

    Camera feed is redirected to CoreML model for classification
    - added comments

--- ImageClassification/ViewController.swift
+++ ImageClassification/ViewController.swift
@@ -53,9 +53,15 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         imageView.layer.addSublayer(previewLayer)
         
         /*
-         Prepare for processing
+         README
          
          To output the frames of the video feed, we need to create an AVCaptureVideoDataOutput.
+         
+         A pixel buffer is a way to store a small amount of image data shortly before it needs to be used.
+         In this app, we’ll use a pixel buffer to store individual frames of our video feed to later feed them into the image classification model.
+         
+         AVCaptureVideoDataOutputSampleBufferDelegate helps with extracting individual frames from video output and transform them into pixel buffer, that is compatible with
+         our Core ML model.
          
          */
         
@@ -65,5 +71,41 @@ extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
         session.addOutput(output)
         session.addInput(input)
         session.startRunning()
+    }
+    
+    // The sampleBuffer parameter of this method is where the delegate passes in an image buffer.
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        // cast the camera output (image) buffer into pixel buffer
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // create a VNCoreMLModel version of the MLmodel you imported in your project
+        guard let model = try? VNCoreMLModel(for: MobileNetV2(configuration: MLModelConfiguration()).model) else { return }
+        
+        // create a request
+        let request = VNCoreMLRequest.init(model: model) { [self] (data, error) in
+            guard error == nil else { return }
+            guard let result = data.results as? [VNClassificationObservation] else { return }
+            guard let firstObject = result.first else { return }
+            
+            DispatchQueue.main.async {
+                if firstObject.confidence * 100 >= 20 {
+                  
+                    self.predictionLabel.text = firstObject.identifier.capitalized
+                    self.confidenceLabel.text = String(firstObject.confidence * 100) + "%"
+                    
+                } else {
+                    self.predictionLabel.text = "--"
+                    self.confidenceLabel.text = "--"
+                }
+            }
+        }
+        
+        // run your request
+        do {
+            try VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        } catch (let error) {
+            print("VNImageRequestHandler thrown an error: \(error.localizedDescription)")
+        }
+       
     }
 }
